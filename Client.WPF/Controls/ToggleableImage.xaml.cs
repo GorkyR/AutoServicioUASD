e7dbf5944392fa0168e7d774efdf278a3b49f1fe
Gorky Rojas <1gorky3rojas@gmail.com>
2019-01-10 16:20:51 -0400

    We have toggleable images

---
+++ Client.WPF/Controls/ToggleableImage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client.WPF.Controls
+{
+    /// <summary>
+    /// Interaction logic for ToggleableImage.xaml
+    /// </summary>
+    public partial class ToggleableImage : UserControl
+    {
+        public ImageSource Image { get; set; }
+        public ImageSource AlternativeImage { get; set; }
+        private bool _estado;
+        public bool Estado { get => _estado;
+            set {
+                _estado = value;
+                if (Estado) {
+                    I1.Visibility = Visibility.Visible;
+                    I2.Visibility = Visibility.Collapsed;
+                } else {
+                    I1.Visibility = Visibility.Collapsed;
+                    I2.Visibility = Visibility.Visible;
+                }
+            }
+        }
+
+        public ToggleableImage()
+        {
+            InitializeComponent();
+        }
+    }
+}
