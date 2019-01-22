9d8b0604272f139b2d7f5b3a5bfd6cc1c1819060
Gorky Rojas <1gorky3rojas@gmail.com>
2019-01-22 15:39:01 -0400

    Schedule page now shows current time

--- Client.WPF/Controls/Schedule.xaml.cs
+++ Client.WPF/Controls/Schedule.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using UASD;
 
 namespace Client.WPF.Controls
@@ -21,12 +23,31 @@ namespace Client.WPF.Controls
     /// </summary>
     public partial class Schedule : UserControl
     {
+        public bool ShowCurrentTime {
+            get => (bool)this.GetValue(ShowCurrentTypeProperty);
+            set => this.SetValue(ShowCurrentTypeProperty, value);
+        }
+        public static readonly DependencyProperty ShowCurrentTypeProperty = DependencyProperty.Register(
+            "ShowCurrentTime", typeof(bool), typeof(Schedule), new PropertyMetadata(false,
+                (obj, args) => {
+                    var schedule = obj as Schedule;
+                    if ((bool)args.NewValue)
+                        schedule.IndicateCurrentTime();
+                    else
+                        schedule.ClearTimeIndicator();
+                }
+            )
+        );
+
         private List<ScheduleItem> Items = new List<ScheduleItem>();
         private List<ScheduleItem> ShadowItems = new List<ScheduleItem>();
+        private DispatcherTimer IndicatorTimer = null;
+        private CurrentTimeIndicator Indicator = null;
 
         public Schedule()
         {
             InitializeComponent();
+            IndicateCurrentTime();
         }
 
         public void LoadCourses(IEnumerable<Course> courses, bool isShadow = false)
@@ -80,11 +101,54 @@ namespace Client.WPF.Controls
                 GSchedule.Children.Remove(item);
             ShadowItems.Clear();
         }
-
+        public void ClearTimeIndicator()
+        {
+            IndicatorTimer?.Stop();
+            GSchedule.Children.Remove(Indicator);
+            IndicatorTimer = null;
+            Indicator = null;
+        }
         public void Clear()
         {
             ClearRegular();
             ClearShadow();
+            ClearTimeIndicator();
+        }
+
+        public void IndicateCurrentTime()
+        {
+            ClearTimeIndicator();
+
+            int slotFromTime  (DateTime time) => time.Minute / 5;
+            int columnFromTime(DateTime time) => (int)time.DayOfWeek;
+            int rowFromTime   (DateTime time) => time.Hour - 6;
+
+            void setupindicator(CurrentTimeIndicator cTIndicator) {
+                var now = DateTime.Now;
+
+                cTIndicator.Visibility = (now.DayOfWeek == DayOfWeek.Sunday || now.Hour < 7 || now.Hour > 21)
+                    ? Visibility.Collapsed
+                    : Visibility.Visible;
+
+                cTIndicator.Slot = slotFromTime(now);
+                Grid.SetColumn(cTIndicator, columnFromTime(now));
+                Grid.SetRow   (cTIndicator, rowFromTime   (now));
+            }
+
+            var indicator = new CurrentTimeIndicator();
+            Panel.SetZIndex(indicator, 1);
+            setupindicator(indicator);
+
+            IndicatorTimer = new DispatcherTimer(
+                interval: TimeSpan.FromSeconds(5),
+                priority: DispatcherPriority.Background,
+                callback: (s, args)  => setupindicator(indicator),
+                dispatcher: this.Dispatcher
+            );
+            IndicatorTimer.Start();
+
+            Indicator = indicator;
+            GSchedule.Children.Add(indicator);
         }
     }
 }
