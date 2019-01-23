d304475b77360192cf37d6f6b7ae4913f294f9e4
Gorky Rojas <1gorky3rojas@gmail.com>
2019-01-23 17:08:16 -0400

    ScheduleWidget working!!

--- Client.WPF/Widgets/ScheduleWidget.xaml.cs
+++ Client.WPF/Widgets/ScheduleWidget.xaml.cs
@@ -20,9 +20,61 @@ namespace Client.WPF.Widgets
     /// </summary>
     public partial class ScheduleWidget : UserControl
     {
+        public string Weekday { get; set; }
+        public static readonly DependencyProperty WeekdayProperty = DependencyProperty.Register(
+            "Weekday", typeof(string), typeof(ScheduleWidget), new PropertyMetadata(
+                UASD.Utilities.Convert.Day(DateTime.Now.DayOfWeek)
+            )
+        );
+
         public ScheduleWidget()
         {
             InitializeComponent();
+        }
+
+        private async void DidLoad(object sender, RoutedEventArgs e)
+        {
+            Cursor = Cursors.AppStarting;
+            var now = DateTime.Now;
+            var today = now.DayOfWeek;
+            var courses = await ClientService.ScheduleAsync();
+
+            var todaysCourses =
+                (from course in courses
+                    select (from instance in course.ScheduleInfo
+                            where instance.DayOfWeek == today
+                            select (Course: course, Instance: instance)))
+                .Aggregate((a, b) => a.Concat(b)).ToList();
+            todaysCourses.Sort((a, b) =>
+                (a.Instance.StartTime - b.Instance.StartTime).Hours
+            );
+            var upcomingClasses = todaysCourses.SkipWhile(courseInstance =>
+                courseInstance.Instance.EndTime < now.TimeOfDay
+            );
+            ICClasses.ItemsSource =
+                from uC in upcomingClasses
+                select new StackPanel {
+                    Children = {
+                        new TextBlock {
+                            Text = $"{UASD.Utilities.Convert.Time(uC.Instance.StartTime)}",
+                            FontSize = 10, FontWeight = FontWeights.Medium,
+                            Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#555"))
+                        },
+                        new Controls.ScheduleItem()
+                        {
+                            Titulo  = uC.Course.Title,
+                            Codigo  = uC.Course.Code,
+                            Lugar   = UASD.Utilities.Convert.Place(uC.Instance.Place),
+                            ToolTip = $"{uC.Course.Title}\n" +
+                                        $"{uC.Course.Code}\n"+
+                                        $"NRC: {uC.Course.NRC}\n" +
+                                        $"{uC.Course.Credits} creditos\n"+
+                                        $"Lugar: {uC.Instance.Place}\n"+
+                                        $"Prof.: {uC.Course.Professor}"
+                        }
+                    }
+                };
+            Cursor = Cursors.Arrow;
         }
     }
 }
