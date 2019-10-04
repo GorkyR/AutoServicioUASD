94ec5636f68dd067debfd12de2b06deda9c6eba6
Gorky Rojas <1gorky3rojas@gmail.com>
2019-10-04 17:19:05 -0400

    Refactored some views using compound TextViews

--- Client.Droid/ProjectionView.cs
+++ Client.Droid/ProjectionView.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
+﻿using Android.Content;
 using Android.Views;
 using Android.Widget;
 using UASD;
@@ -19,11 +11,11 @@ namespace Client.Droid
         {
             Inflate(context, Resource.Layout.view_projection, this);
 
-            var layoutUnavailabe = FindViewById<LinearLayout>(Resource.Id.layout_unavailable);
+            var textUnavailable = FindViewById<TextView>(Resource.Id.text_unavailable);
             var layoutProjection = FindViewById<LinearLayout>(Resource.Id.layout_projection_list);
 
             if (projection.Count == 0)
-                layoutUnavailabe.Visibility = ViewStates.Visible;
+                textUnavailable.Visibility = ViewStates.Visible;
             else 
                 foreach (Course course in projection)
                     layoutProjection.AddView( new ProjectionItemView(context, course) );
