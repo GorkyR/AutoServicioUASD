fd118707211d9b50fab79ca79f5e00e704eefca7
Gorky Rojas <1gorky3rojas@gmail.com>
2020-08-17 21:36:14 -0400

    Implemented selection calendar fetching
    Added exception for expired NIP
    
    Handled credits string edge case

--- Client.Core/Properties/Strings.cs
+++ Client.Core/Properties/Strings.cs
@@ -15,6 +15,7 @@
         public const string KardexUrl                   = "http://www.autoservicio.uasd.edu.do/PROD/bwskotrn.P_ViewTran"; // ?levl=&tprt=INTL
         public const string RegistroSeleccionUrl        = "http://www.autoservicio.uasd.edu.do/PROD/bwskfreg.P_AltPin";
         public const string RegistroUrl                 = "http://www.autoservicio.uasd.edu.do/PROD/bwckcoms.P_Regs";
+        public const string CalendarioSeleccionUrl      = "http://www.autoservicio.uasd.edu.do/PROD/bwskrsta.P_RegsStatusDisp";
 
         public const string BuscarClasesDummyParameters = "&sel_ptrm=%25&begin_hh=0&begin_mi=0&end_hh=0&end_mi=0&begin_ap=x&end_ap=y&path=1&SUB_BTN=Buscar+Curso&SEL_CRSE=&SEL_TITLE=&SEL_DAY=&SEL_SCHD=&SEL_SESS=&SEL_INSTR=&SEL_ATTR=&SEL_LEVL=&SEL_INSM=&CRN=&RSTS=0";
         public const string ClasesDummyParameters       = "&path=1&SUB_BTN=Ver+Secciones+Existentes+&BEGIN_HH=0&BEGIN_MI=0&BEGIN_AP=a&END_HH=0&END_MI=0&END_AP=a&SEL_INSTR=%25&SEL_ATTR=%25&SEL_LEVL=%25&SEL_TITLE=&SEL_DAY=&SEL_PTRM=&SEL_CAMP=&SEL_SCHD=&SEL_SESS=&SEL_INSM=&CRN=&RSTS=&";
@@ -22,5 +23,6 @@
         public const string NotLoggedInMessage          = "Usted ha sido desconectado del AutoServicio.\nDebe ingresar nuevamente.\n\nEsto sucede cuando ingresa al AutoServicio a travez de la pagina principal u otra instancia del cliente, o cuando no interactua por mas de media hora.";
         public const string NoProyectionMessage         = "Su proyeccion no esta disponible en este momento.";
         public const string NoSelectionMessage          = "No es posible seleccionar en estos momentos.";
+        public const string ExpiredLoginMessage         = "Su NIP ha expirado. Proceda al portal web del autoservicio para cambiarlo.";
     }
 }
